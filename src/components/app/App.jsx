dca79a21d9023fcdb34249959d9b35f8ced59a09
ryanleviathan <ryan.m.carreras@gmail.com>
2021-02-11 08:52:28 -0800

    working Article

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import Search from '../search/Search';
+import Article from '../articles/Article';
 
 export default function App() {
-  return <Search search="hello" onChange={() => {}} />;
+  return (
+    <>
+      <Article 
+        title="Pokemon are real!" 
+        url="https://placekitten.com/g/200/300" 
+        image="https://placekitten.com/g/200/300" />
+      <Search search="hello" onChange={() => {}} />
+    </>
+  );
 }
